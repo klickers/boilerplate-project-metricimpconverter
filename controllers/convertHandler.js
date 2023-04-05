4566c545f1786ff683a86a367556bf32927346fb
klickers <bluebirid228@gmail.com>
2023-04-05 13:59:59 -0700

    add convertHandler.getNum() logic + tests

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,47 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  this.getNum = function (input) {
+    let number = input.match(/[\d.\/]+/);
+
+    if (number == null) return 1;
+    else {
+      if (number[0].split("/").length > 2) return "error";
+      else return parseFloat(eval(number[0]));
+    }
   };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
+
+  this.getUnit = function (input) {
+    let unit = input.match(/[a-zA-Z]+/);
+
+    if (unit == null) return "error";
+    else return unit[0];
   };
-  
-  this.getReturnUnit = function(initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
     let result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.spellOutUnit = function (unit) {
     let result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,60 @@
-const chai = require('chai');
+const chai = require("chai");
 let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-
+suite("convertHandler.getNum() tests", function () {
+  test("getNum() can correctly read a whole number input", function () {
+    assert.equal(
+      convertHandler.getNum("34mi"),
+      34,
+      "whole number not read correctly"
+    );
+  });
+  test("getNum() can correctly read a decimal input", function () {
+    assert.equal(
+      convertHandler.getNum("3.4mi"),
+      3.4,
+      "decimal not read correctly"
+    );
+  });
+  test("getNum() can correctly read a fractional input", function () {
+    assert.equal(
+      convertHandler.getNum("3/4mi"),
+      3 / 4,
+      "fraction not read correctly"
+    );
+  });
+  test("getNum() can correctly read a fractional input with a decimal", function () {
+    assert.equal(
+      convertHandler.getNum("3.25/4mi"),
+      3.25 / 4,
+      "fraction with decimal not read correctly"
+    );
+    assert.equal(
+      convertHandler.getNum("325/4.5mi"),
+      325 / 4.5,
+      "fraction with decimal not read correctly"
+    );
+    assert.equal(
+      convertHandler.getNum("3.25/4.5mi"),
+      3.25 / 4.5,
+      "fraction with decimal not read correctly"
+    );
+  });
+  test("getNum() returns an error on a double fraction", function () {
+    assert.equal(
+      convertHandler.getNum("3/4/5mi"),
+      "error",
+      "double fraction error not caught"
+    );
+  });
+  test("getNum() defaults to a numerical input of 1 when no numerical input is provided", function () {
+    assert.equal(
+      convertHandler.getNum("mi"),
+      1,
+      "default value of 1 not returning"
+    );
+  });
 });
